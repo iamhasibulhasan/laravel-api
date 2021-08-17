d934f46a466103b65cab4309cddf615f29fefe19
imhasibulhasan <hashibul218@gmail.com>
2021-08-17 18:45:23 +0600

    create basic api with controller

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class APIController extends Controller
+{
+    /**
+     * Basic Controller
+     */
+    public function basic(){
+        return "API Controller";
+    }
+}
